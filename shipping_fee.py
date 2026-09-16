4737c062346e46151df6197690bbbcace3704530
ji798 <jisuazom@uamv.edu.ni>
2026-09-16 10:39:46 -0600

    arregle el error de input para las tarifas

--- shipping_fee.py
+++ shipping_fee.py
@@ -17,37 +17,38 @@ def cal_shipping_fee(subtotal = 0.0, zone = "Nacional"):
             return shipping_fee
 
         case _:
-            return "no hay envios disponibles a esa zona"
+            print("Opción inválida. Por favor, seleccione una opción válida.")
             
 
     
 def determinar_zona():
-    print("Seleccione la zona de envío:")
-    print("1. Nacional")
-    print("2. Centroamérica")
-    print("3. Europa")
-    print("4. Norteamérica")
-    print("5. Sudamérica")
-    try:
-        choice = int(input("Ingrese el número correspondiente a la zona: "))
+    while True:
+        print("Seleccione la zona de envío:")
+        print("1. Nacional")
+        print("2. Centroamérica")
+        print("3. Europa")
+        print("4. Norteamérica")
+        print("5. Sudamérica")
+        try:
+            choice = int(input("Ingrese el número correspondiente a la zona: "))
 
-        match choice:
-            case 1:
-                return "Nacional"
-            case 2:
-                return "Centroamerica"
-            case 3:
-                return "Europa"
-            case 4:
-                return "Norteamerica"
-            case 5:
-                return "Sudamerica"
-            case _:
-                print("Opción inválida. Por favor, seleccione una opción válida.")
-    except ValueError:
-        print("Entrada inválida. Por favor, ingrese un número válido.")
+            match choice:
+                case 1:
+                    return "Nacional"
+                case 2:
+                    return "Centroamerica"
+                case 3:
+                    return "Europa"
+                case 4:
+                    return "Norteamerica"
+                case 5:
+                    return "Sudamerica"
+                case _:
+                    print("Opción inválida. Por favor, seleccione una opción válida.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número válido.")
+                
             
-        
 
-        
+            
     
